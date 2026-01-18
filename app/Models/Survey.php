b4f87121d1b3e0d56b5fb3a593f0cbb4f8d4feab
wahyuMar <wahyumarrpl@gmail.com>
2026-01-19 00:40:56 +0800

    Add Survey model, factory, and migration for survey data management

---
+++ app/Models/Survey.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Survey extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'lokasi_pipa_distribusi_lat' => 'decimal:8',
+        'lokasi_pipa_distribusi_long' => 'decimal:8',
+        'panjang_pipa_sr' => 'integer',
+        'lokasi_sr_lat' => 'decimal:8',
+        'lokasi_sr_long' => 'decimal:8',
+        'lokasi_rabatan_lat' => 'decimal:8',
+        'lokasi_rabatan_long' => 'decimal:8',
+        'panjang_rabatan' => 'integer',
+        'lokasi_crossing_lat' => 'decimal:8',
+        'lokasi_crossing_long' => 'decimal:8',
+        'panjang_crossing' => 'integer',
+        'tanggal_survey' => 'datetime',
+    ];
+
+    public function customerRegistration()
+    {
+        return $this->belongsTo(CustomerRegistration::class);
+    }
+}
