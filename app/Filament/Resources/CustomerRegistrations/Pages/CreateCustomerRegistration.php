a47d459d4db896d0e70b0721057b705ac59d701e
wahyuMar <wahyumarrpl@gmail.com>
2026-01-19 00:04:58 +0800

    Add CustomerRegistrationHelper to generate unique 'no_surat' and update form data in CreateCustomerRegistration

--- app/Filament/Resources/CustomerRegistrations/Pages/CreateCustomerRegistration.php
+++ app/Filament/Resources/CustomerRegistrations/Pages/CreateCustomerRegistration.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\CustomerRegistrations\Pages;
 
 use App\Filament\Resources\CustomerRegistrations\CustomerRegistrationResource;
+use App\Helper\CustomerRegistrationHelper;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCustomerRegistration extends CreateRecord
 {
     protected static string $resource = CustomerRegistrationResource::class;
+
+    public function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['no_surat'] = CustomerRegistrationHelper::generateNoSurat();
+        $data['tanggal'] = now()->toDateString();
+        return $data;
+    }
 }
